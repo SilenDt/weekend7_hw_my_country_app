a4a0a4d8bc0778f81f082ea725a693171e4edb40
Eilidh Morone <e.silendt@gmail.com>
2023-01-21 11:01:09 +0000

    connected API to the app

--- src/containers/CountryBox.js
+++ src/containers/CountryBox.js
@@ -1,11 +1,21 @@
 
-import React from "react"
+import React, { useEffect, useState } from "react"
 import CountryList from "../components/CountryList"
 import Header from "../components/Header"
-import Country from "../components/Country"
 
 const CountryBox = () => {
 
+    const [countries, setCountries] = useState([])
+    
+    useEffect(() => {
+        getCountries()
+    }, [])
+
+    const getCountries = () => {
+        fetch("https://restcountries.com/v3.1/all")
+            .then(res => res.json())
+                .then(countriesData => setCountries(countriesData))
+    }
     return (
         <div>
             <h1>CountryBox</h1>
